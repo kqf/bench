bf69f77ffc8403ce8685d0b19b1f6f04ac2a110c
kqf <q.dev.null@gmail.com>
2026-05-28 07:06:22 +0100

    Read the samples

--- bench/main.py
+++ bench/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from dadinhos.generate import make_detection_task
+from modelinhos.sample import read_samples
 
 
 def main(path: Path = Path("data/blobs/annotations.json")):
@@ -11,6 +12,8 @@ def main(path: Path = Path("data/blobs/annotations.json")):
             n_samples=1000,
             n_classes=3,
         )
+
+    print(len(read_samples(path)))
 
 
 if __name__ == "__main__":
