b0ee42152bc72682445097b6db3da15981e829c5
kqf <q.dev.null@gmail.com>
2026-05-30 08:02:21 +0100

    Fix the proejct scturcture (#8)
    * Bump pyproject
    
    * Bump the python version
    
    * Add the dataset
    
    * Update the tests
    
    * Ignore the data files
    
    * Read the samples
    
    * Lint everything
    
    * Fix the project structure
    
    * Add the fitting method

--- bench/main.py
+++ bench/main.py
@@ -1,19 +1,51 @@
 from pathlib import Path
 
+import cv2
 from dadinhos.generate import make_detection_task
+from modelinhos.evaluation import (
+    mean_average_precision,
+    per_sample_metrics,
+    visualize_fp_fn,
+)
+from modelinhos.processing import LabelEncoder
 from modelinhos.sample import read_samples
+from modelinhos.ssd.inference import TorchvisionDetector
+from torchvision.models.detection import (
+    SSDLite320_MobileNet_V3_Large_Weights,
+    ssdlite320_mobilenet_v3_large,
+)
 
 
-def main(path: Path = Path("data/blobs/annotations.json")):
+def main(
+    path: Path = Path("data/blobs/annotations.json"),
+    resolution: tuple[int, int] = (480, 640),  # height, width
+):
     if not path.exists():
         make_detection_task(
             path,
-            resolution=(480, 640),
+            resolution=resolution,
             n_samples=1000,
             n_classes=3,
         )
 
-    print(len(read_samples(path)))
+    model = TorchvisionDetector(
+        resolution=resolution,
+        build_model=ssdlite320_mobilenet_v3_large,
+        weights=SSDLite320_MobileNet_V3_Large_Weights.COCO_V1,
+    )
+
+    train = read_samples(path)
+    le = LabelEncoder().fit(train)
+    # model.fit(le.transform(train)) ~
+    y_pred = model.transform(
+        [cv2.imread(sample.file_name) for sample in train]
+    )
+    y_pred = le.inverse_transform(y_pred)
+    m_ap = mean_average_precision(train, y_pred, l2i=le.l2i)
+    print(m_ap["mAP"].iloc[0])
+
+    aps = per_sample_metrics(train, y_pred, l2i=le.l2i)
+    visualize_fp_fn(aps, i2l=le.i2l)
 
 
 if __name__ == "__main__":
