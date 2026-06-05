4c8342a6aae56cd8a0eaaf41301a7291bc6ab6a9
kqf <q.dev.null@gmail.com>
2026-06-05 06:43:44 +0100

    Fix the broken tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from bench.main import main
@@ -8,5 +10,6 @@ def dataset(tmp_path):
     return tmp_path / "data" / "tests" / "annotations.json"
 
 
-def test_main(dataset):
+@patch("bench.main.cv2.imshow")
+def test_main(imshow, dataset):
     main(dataset, n_samples=100)
